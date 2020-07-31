cf9497876f50ea748d45b91d1cc9af7c86026e1a
Taylor Phillips <taylorphillips133@gmail.com>
2020-07-31 11:58:23 -0700

    begin the formulation of the StylistsController

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controllers
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      -db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    
+  }
+}
